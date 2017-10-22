916753e72f824a2a5eaf17041631deb591e94031
Hugo Garcia <hugo_os_95@hotmail.com>
2017-10-22 12:03:54 -0500

    Added maps and changed tiles.

--- Hardhome/DummyMap1Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tiles" tilewidth="32" tileheight="32" tilecount="1440" columns="40">
- <image source="../../../../Desktop/gfx/Tiles.png" width="1280" height="1152"/>
-</tileset>
---
+++ Tiles, Sprites and Art/TreesTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Trees" tilewidth="32" tileheight="32" tilecount="169" columns="13">
+ <image source="../../../../Downloads/plant repack.png" width="416" height="416"/>
+</tileset>
